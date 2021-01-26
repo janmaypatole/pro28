4e9f428fc9f28da9a4368bb85b8da5a1463ff69c
janmaypatole <74344054+janmaypatole@users.noreply.github.com>
2021-01-26 17:19:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+var stone 
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+   stone = new Stone(200,400,40,50)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+
+  stone.display();
+  drawSprites();
+ 
+}
+
+
+
